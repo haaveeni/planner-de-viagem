9b6a5d57251148d10f8205871b5eda87d6870334
Veridiana Lucena <veridianalucena@gmail.com>
2024-07-11 20:24:02 -0300

    Added: index.css

--- frontendPlanner/src/main.tsx
+++ frontendPlanner/src/main.tsx
@@ -1,9 +1,10 @@
-import React from 'react' //react global: react, react native (mobile), reactTv, etc
-import ReactDOM from 'react-dom/client' //react para web
-import { App } from './app'
+import React from "react"; //react global: react, react native (mobile), reactTv, etc
+import ReactDOM from "react-dom/client"; //react para web
+import { App } from "./app";
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
