b29bc04e58b813b4b243b65255ceb3f40f1f742b
Veridiana Lucena <veridianalucena@gmail.com>
2024-07-15 16:12:56 -0300

    Change: updated functions names

--- frontendPlanner/src/routes/trip-details/index.tsx
+++ frontendPlanner/src/routes/trip-details/index.tsx
@@ -16,10 +16,10 @@ export function TripDetailsPage() {
   const [isCreateActivityModalOpen, setIsCreateActivityModalOpen] =
     useState(false);
 
-  function openActivityRegister() {
+  function openCreateActivityModal() {
     setIsCreateActivityModalOpen(true);
   }
-  function closeActivityRegister() {
+  function closeCreateActivityModal() {
     setIsCreateActivityModalOpen(false);
   }
 
@@ -51,7 +51,7 @@ export function TripDetailsPage() {
           <div className="flex items-center justify-between">
             <h2 className="text-3xl font-semibold">Atividades</h2>
             <button
-              onClick={openActivityRegister}
+              onClick={openCreateActivityModal}
               className="bg-lime-300 text-lime-950 rounded-lg px-5 py-2 font-medium flex items-center gap-2 hover:bg-lime-400"
             >
               <Plus className="size-5" />
@@ -206,7 +206,7 @@ export function TripDetailsPage() {
             <div className="space-y-2">
               <div className="flex items-center justify-between">
                 <h2 className="text-lg font-semibold">Cadastrar atividade</h2>
-                <button onClick={closeActivityRegister} type="button">
+                <button onClick={closeCreateActivityModal} type="button">
                   <X className="size-5 text-zinc-400" />
                 </button>
               </div>
